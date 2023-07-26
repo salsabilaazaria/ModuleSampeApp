bd381399c46a99c36d6ab2bebc3cb70a32d4437e
Salsabila Azaria <salsabilaazariaa@gmail.com>
2023-07-26 20:59:15 +0700

    first commit

---
+++ ModuleSampleApp/ModuleSampleApp.h
@@ -0,0 +1,18 @@
+//
+//  ModuleSampleApp.h
+//  ModuleSampleApp
+//
+//  Created by OCTO Mobile on 26/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModuleSampleApp.
+FOUNDATION_EXPORT double ModuleSampleAppVersionNumber;
+
+//! Project version string for ModuleSampleApp.
+FOUNDATION_EXPORT const unsigned char ModuleSampleAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleSampleApp/PublicHeader.h>
+
+
