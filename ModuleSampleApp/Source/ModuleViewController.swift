200d62ebaed7cb95bcf7928cd81ef597f4802bcf
Salsabila Azaria <salsabilaazariaa@gmail.com>
2023-07-28 09:29:48 +0700

    update to programmatically UI

--- ModuleSampleApp/Source/ModuleViewController.swift
+++ ModuleSampleApp/Source/ModuleViewController.swift
@@ -1,29 +1,32 @@
 //
-//  ModuleViewController.swift
+//  SampleViewController.swift
 //  ModuleSampleApp
 //
-//  Created by OCTO Mobile on 26/07/23.
+//  Created by OCTO Mobile on 27/07/23.
 //
 
 import UIKit
-
 public class ModuleViewController: UIViewController {
-
-    public override func viewDidLoad() {
+    
+   public override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let mainView = UIView(frame: UIScreen.main.bounds)
+        mainView.backgroundColor = .lightGray
+        
+        let label = UILabel()
+        label.text = "This is ModuleViewController"
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        mainView.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.centerXAnchor.constraint(equalTo: mainView.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: mainView.centerYAnchor).isActive = true
+ 
+        
+        self.view.addSubview(mainView)
+        
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
+
